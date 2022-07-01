39882fe1a7da9534d428bb93a64711da421d98fe
baibaixy <2942583916@qq.com>
2022-07-01 19:48:52 +0800

    附加练习

---
+++ src/2 加加减减.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(item, index) in arr" :key="index">
+        <span>{{ item }}</span>
+        <button @click="del(index)">删除</button>
+      </li>
+    </ul>
+    <button @click="add">生成</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [1, 5, 3],
+    };
+  },
+  methods: {
+    add() {
+      this.arr.push(Math.floor(Math.random() * 20));
+    },
+    del(ind) {
+      this.arr.splice(ind, 1);
+    },
+  },
+};
+</script>
