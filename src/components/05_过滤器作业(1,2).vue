3f99b26b3620cf36575f4618b35c1831f7ded68f
13frfr <2458151214@qq.com>
2022-07-01 21:09:04 +0800

    增加并完成了过滤器作业1，2

--- src/components/05_过滤器作业(1,2).vue
+++ src/components/05_过滤器作业(1,2).vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    国内网站：粉丝数量{{ beauty | girls }} <br>
+    国内网站：粉丝数量{{ beauty | girls }} <br />
     国内网站：粉丝数量{{ beauty | girlplus }}
+    时间：{{ ugly | boys }} <br />
+    时间：{{ ugly | boyplus }}
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 export default {
   data() {
     return {
       beauty: '1876986',
+      ugly: +new Date(),
     }
   },
   filters: {
@@ -18,6 +22,12 @@ export default {
     },
     girlplus(val) {
       return val / 1000 + '千'
+    },
+    boys(val) {
+      return moment(val).format('YYYY-MM-DD')
+    },
+    boyplus(val) {
+      return moment(val).format('YYYY/MM/DD')
     },
   },
 }
