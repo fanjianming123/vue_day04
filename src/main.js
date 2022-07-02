e13719289d2aa976a2460e8aa9576be4653afa17
13frfr <2458151214@qq.com>
2022-07-02 15:01:17 +0800

    完成了购物车案例

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
